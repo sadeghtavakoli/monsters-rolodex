e42442c7131a116eaf651ea1d9a8f7adf9839b79
Sadegh Tavakoli <sadeghtavakoli752@gmail.com>
2021-08-16 13:32:19 +0430

    creating card-list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchText: "",
+    };
+  }
+  componentDidMount() {
+    const loadMonsters = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const monsters = await response.json();
+
+      this.setState({ monsters });
+    };
+
+    loadMonsters();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="app-header">Monsters Rolodex</h1>
+        <SearchBox text={this.state.searchText} />
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
